9eac0dbcc2c4ee1dbf3e98a308499d00068e957c
Kolya lukin <mykola@imagecms.net>
2018-04-25 15:18:12 +0300

    fix bug

--- src/PriceSpyPlugin.php
+++ src/PriceSpyPlugin.php
@@ -1,6 +1,7 @@
 <?php namespace Addon\PriceSpy;
 
 use Premmerce\SDK\V1\FileManager\FileManager;
+use Premmerce\SDK\V1\Notifications\AdminNotifier;
 use Addon\PriceSpy\Admin\Admin;
 use Addon\PriceSpy\Frontend\Frontend;
 
@@ -23,8 +24,10 @@ class PriceSpyPlugin {
 	 */
     public function __construct($mainFile) {
         $this->fileManager = new FileManager($mainFile);
+        $this->notifier    = new AdminNotifier();
 
-        add_action('init', [ $this, 'loadTextDomain' ]);
+        add_action('plugins_loaded', [ $this, 'loadTextDomain' ]);
+        add_action('admin_init', [$this, 'checkRequirePlugins']);
 
 	}
 
@@ -32,10 +35,16 @@ class PriceSpyPlugin {
 	 * Run plugin part
 	 */
 	public function run() {
-		if ( is_admin() ) {
-			new Admin( $this->fileManager );
-		} else {
-			new Frontend( $this->fileManager );
+
+		$valid = count( $this->validateRequiredPlugins() ) === 0;
+		
+		if( $valid ){
+			if ( is_admin() ) {
+				new Admin( $this->fileManager );
+			} else {
+				new Frontend( $this->fileManager );
+			}
+
 		}
 
 	}
@@ -48,6 +57,55 @@ class PriceSpyPlugin {
         $name = $this->fileManager->getPluginName();
         load_plugin_textdomain('addon-price-spy', false, $name . '/languages/');
     }
+
+    /**
+     * Check required plugins and push notifications
+     */
+    public function checkRequirePlugins(){
+        $message = __( 'The %s plugin requires %s plugin to be active!', 'addon-price-spy' );
+
+        $plugins = $this->validateRequiredPlugins();
+
+        if( count( $plugins ) ){
+            foreach($plugins as $plugin){
+                $error = sprintf($message, 'Premmerce Price Spy addon', $plugin);
+                $this->notifier->push($error, AdminNotifier::ERROR, false);
+            }
+        }
+
+    }
+
+	/**
+	 * Validate required plugins
+	 *
+	 * @return array
+	 */
+	private function validateRequiredPlugins(){
+
+		$plugins = [];
+
+		if( ! function_exists('is_plugin_active' ) ){
+			include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+		}
+
+		/**
+		 * Check if WooCommerce is active
+		 **/
+		if(!(is_plugin_active('woocommerce/woocommerce.php') || is_plugin_active_for_network('woocommerce/woocommerce.php'))){
+			$plugins[] = '<a target="_blank" href="https://wordpress.org/plugins/woocommerce/">WooCommerce</a>';
+		}
+
+		/**
+		 * Check if Price spy is active
+		 **/
+		if(! ( is_plugin_active( 'premmerce-price-spy/premmerce-price-spy.php' ) || 
+			is_plugin_active_for_network( 'premmerce-price-spy/premmerce-price-spy.php' )
+		)){
+			$plugins[] = '<a target="_blank" href="#">Premmerce Price Spy</a>';
+		}
+
+		return $plugins;
+	}
 
 	/**
 	 * Fired when the plugin is activated
--- views/frontend/modal-inputs.php
+++ views/frontend/modal-inputs.php
@@ -1,4 +1,6 @@
 <?php if ( ! defined( 'WPINC' ) ) die; ?>
 
-<label for="percent"><?php _e( 'Percent change', 'addon-price-spy' )?>: </label>
-<input type="text" name="data[percent]" placeholder="optional" id="percent"></br>
+<p class="form-row form-row-wide">
+	<label for="percent"><?php _e( 'Percent', 'addon-price-spy' )?>: </label>
+	<input type="text" name="data[percent]" placeholder="optional" id="percent" class="input-text"></br>
+</p>
